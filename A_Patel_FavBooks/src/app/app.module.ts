be6d8c2a13b605e8a4084e4e643f826afa1ba04b
Anjali4006 <105735999+Anjali4006@users.noreply.github.com>
2023-02-08 14:56:00 -0500

    Commit all

--- A_Patel_FavBooks/src/app/app.module.ts
+++ A_Patel_FavBooks/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContentCardComponent } from './component-card/component-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentCardComponent
   ],
   imports: [
     BrowserModule
